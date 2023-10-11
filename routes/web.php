367892a974effdcec34e97bb3bc4c76c31e45080
mauroluciano <mauroluciano911@gmail.com>
2023-10-11 17:06:25 +0200

    hello

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $hello = "Hello world";
+
+
+    return view('home', [
+        'hello' => $hello, 
+    ]);
 });
